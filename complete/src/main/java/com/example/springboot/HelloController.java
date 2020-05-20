907db7db2c5308b60c6f6f9502c1a8e774592a11
hieudao <hieudao@hoiio.com>
2020-05-20 12:34:13 +0700

    test custom and simple Java Client Prometheus - Custom Metrics

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -2,13 +2,45 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+
+// add Prometheus
+import io.prometheus.client.exporter.MetricsServlet;
+import io.prometheus.client.hotspot.DefaultExports;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// add CustomMetrics
+import com.example.springboot.HelloPromCustomMetrics2;
+import com.example.springboot.HelloPromCustomMetrics1;
 
 @RestController
+@Configuration
 public class HelloController {
 
 	@RequestMapping("/")
+	@ResponseStatus(HttpStatus.OK)
 	public String index() {
-		return "Greetings from Spring Boot!";
+		HelloPromCustomMetrics1.reqInc();
+		return "<a href=\"/metrics\">Metrics</a>";
 	}
 
+	@RequestMapping(value="**",method = RequestMethod.GET)
+	public String getAnythingelse(){
+		HelloPromCustomMetrics1.reqInc();
+		// return "everythingelse";
+		return "<a href=\"/metrics\">Metrics</a>";
+	}
+
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean() {
+		DefaultExports.initialize();
+		return new ServletRegistrationBean(new MetricsServlet(), "/metrics");
+	}
 }
---
+++ complete/src/main/java/com/example/springboot/HelloPromCustomMetrics1.java
@@ -0,0 +1,15 @@
+package com.example.springboot;
+
+import io.prometheus.client.Counter;
+import io.prometheus.client.Gauge;
+import io.prometheus.client.Collector;
+
+public class HelloPromCustomMetrics1 {
+    private static final Counter countReq = Counter.build()
+            .name("countReq").help("countReq description help")
+            .labelNames("countReq").register();
+
+    public static void reqInc() {
+        countReq.labels("countReq").inc();
+    }
+}
